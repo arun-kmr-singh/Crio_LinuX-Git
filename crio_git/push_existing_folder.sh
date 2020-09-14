f27b0809955ab7725fdd3b39eb46379b4e6a03ec
ARUN KUMAR SINGH <2019pcs0017@iitjammu.ac.in>
2020-09-15 01:19:16 +0530

    COmmit after Git Basics Milestone 2 - Submodule added

---
+++ crio_git/push_existing_folder.sh
@@ -0,0 +1,6 @@
+cd existing_folder
+git init
+git remote add origin git@gitlab.crio.do:COHORT_ME_GIT_BASICS_ENROLL_1596802014715/crio-2019pcs0017-iitjammu-ac-me_git_basics.git
+git add .
+git commit -m "Initial commit"
+git push -u origin master
